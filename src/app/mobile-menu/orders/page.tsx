6566dfa5d3a2f6dbeb37fb12f40f056d247234cd
nhattruong22690-dotcom <nhat.truong22690@gmail.com>
2026-03-26 10:58:04 +0700

    fix: resolve syntax error in OrdersMenuPage causing Vercel build failure

--- src/app/mobile-menu/orders/page.tsx
+++ src/app/mobile-menu/orders/page.tsx
@@ -15,7 +15,7 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs);
+  return twMerge(clsx(inputs));
 }
 
 const items = [
